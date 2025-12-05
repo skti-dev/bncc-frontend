170f6b2a8a0f513204f30dcf93a0068c270c6db7
Augusto Seabra <augusto.seabra00@gmail.com>
2025-12-04 21:56:09 -0300

    fix: update API_URL to use the correct backend URL

--- constants/api.ts
+++ constants/api.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API_URL || 'http://192.168.0.10:8000';
-// export const API_URL = process.env.API_URL || 'https://bncc-backend.onrender.com';
+// export const API_URL = process.env.API_URL || 'http://192.168.0.10:8000';
+export const API_URL = process.env.API_URL || 'https://bncc-backend.onrender.com';
